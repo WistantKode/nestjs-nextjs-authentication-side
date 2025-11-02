9a84313cbb4bec5da80e9b5f4a370f71d6005ed5
WistantKode <wistantkode@protonmail.com>
2025-11-02 08:17:54 +0100

    feat/web/auth/signup: create page.tsx file and edit

--- apps/web/app/auth/layout.tsx
+++ apps/web/app/auth/layout.tsx
@@ -1,11 +1,11 @@
-import React, { PropsWithChildren } from 'react'
+import React, { PropsWithChildren } from "react";
 
-const AuthLayout = ({children}: PropsWithChildren) => {
+const AuthLayout = ({ children }: PropsWithChildren) => {
   return (
-    <div>
-        {children}
+    <div className="bg-gradient-to-br from-lime-400 to-cyan-400 h-screen flex items-center justify-center">
+      {children}
     </div>
-  )
-}
+  );
+};
 
-export default AuthLayout
+export default AuthLayout;
